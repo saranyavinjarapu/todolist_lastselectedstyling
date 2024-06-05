90c3b24a61b6b57122ff62854140a87620144af0
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2024-06-05 14:08:38 +0530

    added created at and completed at fields to display respective timestamps

--- examples/react/src/todo/components/header.jsx
+++ examples/react/src/todo/components/header.jsx
@@ -4,12 +4,23 @@ import { Input } from "./input";
 import { ADD_ITEM } from "../constants";
 
 export function Header({ dispatch }) {
-    const addItem = useCallback((title) => dispatch({ type: ADD_ITEM, payload: { title } }), [dispatch]);
+	const addItem = useCallback(
+		(title) => dispatch({ type: ADD_ITEM, payload: { title } }),
+		[dispatch]
+	);
 
-    return (
-        <header className="header" data-testid="header">
-            <h1>todos</h1>
-            <Input onSubmit={addItem} label="New Todo Input" placeholder="What needs to be done?" />
-        </header>
-    );
+	return (
+		<header className="header" data-testid="header">
+			<h1>todos</h1>
+			<div className="header-columns">
+				<Input
+					onSubmit={addItem}
+					label="New Todo Input"
+					placeholder="What needs to be done?"
+				/>
+				<div className="time-stamp">Created</div>
+				<div className="time-stamp">Completed</div>
+			</div>
+		</header>
+	);
 }
